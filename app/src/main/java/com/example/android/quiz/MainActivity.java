3c9b4585e949117dca474218d57878815acde23b
Chrisse Tiana <chrissetiana@gmail.com>
2018-01-12 13:52:18 +0100

    Update layout

--- app/src/main/java/com/example/android/quiz/MainActivity.java
+++ app/src/main/java/com/example/android/quiz/MainActivity.java
@@ -1,7 +1,12 @@
+/**
+ * if-else
+ * toast msg with score when grading button is clicked
+ * grading logic: check if correct ans is checked AND incorrect ones are not checked
+ */
 package com.example.android.quiz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
